65f23a78ff97e3fd4bdf869718e5e4d25941aeaa
Gui-dev <adrianon10@gmail.com>
2026-03-06 20:07:46 -0300

    feat(search): add loading skeleton for search results page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,7 +1,25 @@
+import { Skeleton } from '@/components/skeleton'
+
 const SearchLoading = () => {
   return (
-    <div>
-      <h1>Loading Search...</h1>
+    <div className="flex flex-col gap-4">
+      <Skeleton className="h-5 w-64" />
+
+      <div className="grid grid-cols-3 gap-6">
+        {[1, 2, 3, 4, 5, 6].map(id => (
+          <div
+            key={`search-skeleton-${id}`}
+            className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+          >
+            <Skeleton className="h-80 w-80" />
+
+            <div className="absolute right-10 bottom-10 flex max-w-70 items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <Skeleton className="h-4 w-24" />
+              <Skeleton className="h-full w-16 rounded-full" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
