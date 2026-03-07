af7c29fbd1f7a7525b264aff0a1091ecb8d59c73
Gui-dev <adrianon10@gmail.com>
2026-03-07 20:26:28 -0300

    refactor(header): extract search form to separate component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
-import { Search } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { CartWidget } from './cart-widget'
+import { SearchForm } from './search-form'
 
 export const Header = () => {
   return (
@@ -10,13 +10,7 @@ export const Header = () => {
         <Link href="/" title="Dev Store Home">
           <h1 className="font-extrabold text-2xl text-white">Dev Store</h1>
         </Link>
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="size-5 text-zinc-500" />
-          <input
-            className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-            placeholder="Buscar produto..."
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
         <CartWidget />
